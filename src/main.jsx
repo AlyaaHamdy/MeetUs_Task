dbd307c5c7608c5a96d83f8e6d4c8dd77a34c4f7
alyaaHamdy <alyaa.hamdy@outlook.com>
2024-07-02 10:21:29 +0300

    Refactor the code

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import StoreContextProvider from './components/context/StoreContext.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
